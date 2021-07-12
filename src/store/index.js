3c48c7cb53e1619301eae779773650ff26cffa0b
Alan <alanbschio@gmail.com>
2021-07-12 01:18:33 -0400

    feat: store for user

--- src/store/index.js
+++ src/store/index.js
@@ -12,19 +12,27 @@ const store = new Vuex.Store({
         value: 1000.0,
       },
     ],
+    user: null,
   },
   mutations: {
     ADD_CART(state, cartProdut) {
       state.cart.push(cartProdut);
+    },
+    SET_USER(state, user) {
+      state.user = user;
     },
   },
   actions: {
     addToCard(context, cartProdut) {
       context.commit("ADD_CART", cartProdut);
     },
+    setUser(context, user) {
+      context.commit("SET_USER", user);
+    },
   },
   getters: {
     cart: (state) => state.cart,
+    user: (state) => state.user,
   },
 });
 
